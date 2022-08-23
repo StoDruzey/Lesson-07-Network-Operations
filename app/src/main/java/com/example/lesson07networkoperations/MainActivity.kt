54c88a8f4b2dbe9768bc708359d8f2ecb4887770
Serge Labada <sergelabada@gmail.com>
2022-08-23 23:51:48 +0300

    Two ways to create dialog

--- app/src/main/java/com/example/lesson07networkoperations/MainActivity.kt
+++ app/src/main/java/com/example/lesson07networkoperations/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.lesson07networkoperations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+//these are not network exercices. These are about fragments, transferring data between fragments
+//and dialogs
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
